71a41411ad006c6045491a6c3a5d297b2c437e8d
hvdx23 <hv25@students.waikato.ac.nz>
2025-06-05 00:00:45 +1200

    class created. working on parameter insertion

--- main.py
+++ main.py
@@ -1,9 +1,14 @@
 from openmateo import get_weather
+from openmateoclass import WeatherAPI
 
 latitude= [-37.78,52.48]
 longitude=[175.27,45.44]
 parameters=[{}]
 days=1
 
-data=get_weather(latitude,longitude,days)
+# data=get_weather(latitude,longitude,days)
+weather_api = WeatherAPI()
+
+aus_data=weather_api.get_weather(56.23,45.21,10)
+
 # print(data)
---
+++ openmateoclass.py
@@ -0,0 +1,50 @@
+import openmeteo_requests
+import pandas as pd
+import requests_cache
+from retry_requests import retry
+
+cache_session = requests_cache.CachedSession('.cache', expire_after = 3600)
+retry_session = retry(cache_session, retries = 5, backoff_factor = 0.2)
+openmeteo = openmeteo_requests.Client(session = retry_session)
+
+class WeatherAPI:
+    
+    def __init__(self):
+        # Initialize the Open-Meteo API client with cache and retry settings
+        cache_session = requests_cache.CachedSession('.cache', expire_after=3600)
+        retry_session = retry(cache_session, retries=5, backoff_factor=0.2)
+        self.client = openmeteo_requests.Client(session=retry_session)
+        print("ClientAPI done")
+
+
+    def get_weather(self, latitude, longitude, days):
+        url = "https://api.open-meteo.com/v1/forecast"
+        params = {
+            "latitude": latitude,
+            "longitude": longitude,
+            "hourly": ["temperature_2m", "relative_humidity_2m"],
+            "timezone": "Australia/Sydney",
+            "forecast_days": days
+        }
+        print(f'Lat={latitude}, Long={longitude}')
+        responses = self.client.weather_api(url, params=params)
+
+        response = responses[0]
+        hourly = response.Hourly()
+        hourly_temperature_2m = hourly.Variables(0).ValuesAsNumpy()
+        hourly_relative_humidity_2m = hourly.Variables(1).ValuesAsNumpy()
+
+        hourly_data = {
+            "date": pd.date_range(
+                start=pd.to_datetime(hourly.Time(), unit="s", utc=True),
+                end=pd.to_datetime(hourly.TimeEnd(), unit="s", utc=True),
+                freq=pd.Timedelta(seconds=hourly.Interval()),
+                inclusive="left"
+            ),
+            "temperature_2m": hourly_temperature_2m,
+            "relative_humidity_2m": hourly_relative_humidity_2m
+        }
+
+        hourly_dataframe = pd.DataFrame(data=hourly_data)
+        print(hourly_dataframe)
+        return hourly_dataframe
